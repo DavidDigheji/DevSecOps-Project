0442366a62e073cec64db19c4573318d9e63a042
david <d.digheji@gmail.com>
2024-12-21 09:20:28 +0000

    update

--- main.tf
+++ main.tf
@@ -9,3 +9,13 @@ resource "aws_s3_bucket" "terraformjuly2026" {
   bucket = "terraformjuly2026"
 }
 
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket    = "terraformjuly2024"
+    key       = "global/s3/terraform.tfstate"
+    region    = "eu-west-2"
+
+  }
+}
+
