ecb42644659fb30c85bbd5145449b7d1b767b345
david <d.digheji@gmail.com>
2024-12-21 09:58:22 +0000

    update

--- main.tf
+++ main.tf
@@ -1,21 +1,14 @@
 provider "aws" {
     region = "eu-west-2"
 }
-resource "aws_s3_bucket" "terraformjuly" {
-  bucket = "terraformjuly2024"
+resource "aws_s3_bucket" "terraform-project2025" {
+  bucket = "terraform-project-2025"
 }
-
-resource "aws_s3_bucket" "terraformjuly2026" {
-  bucket = "terraformjuly2026"
-}
-
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket    = "terraformjuly2024"
+    bucket    = "terraform-project-2025"
     key       = "global/s3/terraform.tfstate"
     region    = "eu-west-2"
-
   }
 }
-
